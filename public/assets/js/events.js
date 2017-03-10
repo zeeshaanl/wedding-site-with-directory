6aae05cc91e93ca1f47b9888ce3ec047f7d8c0eb
zeeshaanl <zeeshaan.lakdawala@happycar.de>
2017-03-10 11:03:54 +0100

    modify image

--- public/assets/js/events.js
+++ public/assets/js/events.js
@@ -1,39 +1,45 @@
-var eventsHandler = function () {
-  function init() {
-    attachListener();
-  }
-  
-  function attachListener() {
-    $('#rsvp-form').on('submit',function(e){
-      $("#rsvp-form").attr("disabled", true);
-      e.preventDefault();
-      $('.submit-text').addClass('hidden');
-      $('.submit-loader').removeClass('hidden');
-      
-      $.ajax({
-          type     : "POST",
-          cache    : false,
-          url      : 'http://wedding-site-zeeshaanl699662.codeanyapp.com:8000/rsvp',
-          data     : $(this).serialize(),
-          success  : function(data) {
-              $('.submit-loader').addClass('hidden');
-              $('.submit-success').removeClass('hidden');
-          }
-      });
-    });
-    $('.rsvp-radio input:radio').click(function() {
-      if ($(this).val() === 'Yes') {
-        $('.meal-pref').fadeIn('fast');
-      } else if ($(this).val() === 'No') {
-        $('.meal-pref').fadeOut('fast');
-      }
-    });
-  }
-  return {
-    init: init
-  }
+var eventsHandler = function() {
+    function init() {
+        attachListener();
+    }
+
+    function attachListener() {
+        $('#rsvp-form').on('submit', function(e) {
+            $("#rsvp-form").attr("disabled", true);
+            e.preventDefault();
+            $('.submit-text').addClass('hidden');
+            $('.submit-loader').removeClass('hidden');
+
+            $.ajax({
+                type: "POST",
+                cache: false,
+                url: 'http://wedding-site-zeeshaanl699662.codeanyapp.com:8000/rsvp',
+                data: $(this).serialize(),
+                success: function(data) {
+                    $('.submit-loader').addClass('hidden');
+                    $('.submit-success').removeClass('hidden');
+                }
+            });
+        });
+        $('.rsvp-radio input:radio').click(function() {
+            if ($(this).val() === 'Yes') {
+                $('.meal-pref').fadeIn('fast');
+            } else if ($(this).val() === 'No') {
+                $('.meal-pref').fadeOut('fast');
+            }
+        });
+        $('.rsvp-button').click(function() {
+            $('html,body').animate({
+                scrollTop: $(".inner.medium").offset().top
+            });
+        })
+    }
+
+    return {
+        init: init
+    }
 }();
 
-$(document).ready(function () {
+$(document).ready(function() {
     eventsHandler.init();
 });
