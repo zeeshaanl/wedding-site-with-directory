49cdac2a5633b223953be490775834c66c74d8aa
zeeshaanl <zeeshaan.lakdawala@happycar.de>
2017-08-02 15:30:03 +0200

    add image transition to intro images

--- public/assets/js/events.js
+++ public/assets/js/events.js
@@ -1,45 +1,65 @@
-var eventsHandler = function() {
+const eventsHandler = function () {
+    const handler = {
+        imageContainer: null
+    };
+
     function init() {
+        console.log('in init');
+        initElements();
         attachListener();
+        setImageRotationInterval();
+    }
+
+    function initElements() {
+        handler.imageContainer = $('.parallax');
     }
 
     function attachListener() {
-        $('#rsvp-form').on('submit', function(e) {
+        $('#rsvp-form').on('submit', function (e) {
             $("#rsvp-form").attr("disabled", true);
             e.preventDefault();
             $('.submit-text').addClass('hidden');
             $('.submit-success').addClass('hidden');
 
             $('.submit-loader').removeClass('hidden');
-            var url = window.location.href;
-            console.log(url+ '/rsvp');
+            const url = window.location.href;
+            console.log(url + '/rsvp');
 
             $.ajax({
                 type: "POST",
                 cache: false,
-                url: url+ 'rsvp',
+                url: url + 'rsvp',
                 data: $(this).serialize(),
-                success: function(data) {
+                success: function (data) {
                     $('.submit-loader').addClass('hidden');
                     $('.submit-success').removeClass('hidden');
                     console.log(data);
                 }
             });
         });
-        $('.rsvp-radio input:radio').click(function() {
+        $('.rsvp-radio input:radio').click(function () {
             if ($(this).val() === 'Yes') {
                 $('.meal-pref').fadeIn('fast');
             } else if ($(this).val() === 'No') {
-                $('.meal-pref').fadeOut('fast', function() {
+                $('.meal-pref').fadeOut('fast', function () {
                     $(".meal-pref select option[name='default']").prop('selected', true);
                 });
             }
         });
-        $('.rsvp-button').click(function() {
+        $('.rsvp-button').click(function () {
             $('html,body').animate({
                 scrollTop: $(".inner.medium").offset().top
             });
         })
+    }
+
+    function setImageRotationInterval() {
+        setTimeout(addRemoveClass, 3000)
+    }
+
+    function addRemoveClass() {
+        handler.imageContainer.hasClass('show-second') ? handler.imageContainer.removeClass('show-second') : handler.imageContainer.addClass('show-second');
+        setImageRotationInterval()
     }
 
     return {
@@ -47,6 +67,6 @@ var eventsHandler = function() {
     }
 }();
 
-$(document).ready(function() {
+$(document).ready(function () {
     eventsHandler.init();
 });
